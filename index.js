0dbf81efc537ba0bd08dd81e7a888da7d4014980
Juan8116 <bg1373@est.ort.edu.ar>
2025-09-15 11:30:18 -0300

    Update index.js

--- index.js
+++ index.js
@@ -1,3 +1,11 @@
 import fs from "fs";
-let orden = [71, 1, 2, 116, 3, 84, 4, 5, 81, 74, 6, 7, 8, 93, 68, 9, 104, 10, 65, 11, 136]
-for (let i=0; i < orden.length; i++){
+let cuento = [71, 1, 2, 116, 3, 84, 4, 5, 81, 74, 6, 7, 8, 93, 68, 9, 104, 10, 65, 11, 136]
+let parte;
+let completo = "";
+
+for (let i=0; i < cuento.length; i++){
+    parte =
+fs.readFileSync("Cuento/parte" + cuento [i] + ".txt", "utf-8");
+completo = completo + parte + "\n";
+}
+fs.writeFileSync("La autopista del sur.txt", completo)
